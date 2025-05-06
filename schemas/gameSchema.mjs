dffbb8d8c79a748958483ffbbfdc1d97e9014bb3
m-meno <mackenzie.meno@gmail.com>
2025-05-06 15:12:14 -0600

    Added schemas: user, cart, game

---
+++ schemas/gameSchema.mjs
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const gameSchema = new mongoose.Schema({
+    title:{type: String, required: true},
+    category: 
+    {
+        type: String, 
+        required: true, 
+        enum: ["Board", "Dice", "Outdoor", "Video", "Other"]
+    },
+    price: {type: Number, required: true},
+    desc: {type: String},
+    qty: {type: Number, required: true}
+
+})
+
+export default mongoose.model("Game", gameSchema);
